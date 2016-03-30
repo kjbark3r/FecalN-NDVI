d0515b61f3f91246a2b51026ea0aa2c9134282ee
Kristin Barker <kristinjbarker@gmail.com>
2016-03-30 09:08:55 -0600

    Set wd to either computer; added fecaln data; failed to do much else

--- Barker_LandEcol.R
+++ Barker_LandEcol.R
@@ -6,14 +6,27 @@
 #Goal: Quantify relationship between remotely sensed vegetation
        #index (NDVI or EVI) and elk fecal nitrogen
 
+#read in data
+wd_workcomp <- "C:\\Users\\kristin.barker\\Documents\\GitHub\\FecalN-NDVI"
+wd_laptop <- "C:\\Users\\kjbark3r\\Documents\\GitHub\\FecalN-NDVI"
+
+if (file.exists(wd_workcomp)){
+  inpath="C:\\Users\\kristin.barker\\Documents\\GitHub\\FecalN-NDVI\\"
+} else {
+  inpath="C:\\Users\\kjbark3r\\Documents\\GitHub\\FecalN-NDVI\\"
+}
+
+setwd(inpath)
+getwd() #sanity check
+
+remote.raw <- read.csv("remotedata.csv")
+fecaln.raw <- read.csv("fecalndata2014.csv")
+
 #load libraries
+  ##data wrangling
 library(dplyr)
 library(tidyr)
-
-#read in data
-setwd("C:/Users/kristin.barker/Documents/GitHub/FecalN-NDVI")
-remote.raw <- read.csv("remotedata.csv")
-fecaln.raw <- read.csv("fecalndata.csv")
+  ##spatial stuff
 
 #if you wanna check anything out
 summary(remote.raw)
@@ -25,3 +38,6 @@ str(fecaln.raw)
 remote.data <- filter(remote.raw, Type == "Pellet 2014")
 remote.data$PlotID <- extract_numeric(remote.data$PlotID)
 
+#check out the data
+##hey data, you come here often?
+plot(fecaln.raw$PctFN ~ fecaln.raw$SamplePd)
